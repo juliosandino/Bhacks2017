3d7ea98adb76dd7e89f4a198f39e9cee1a21e50d
rat395 <rat395@ymail.com>
2017-04-23 07:54:54 -0700

    updated database for congressman

--- src/candidates/models.py
+++ src/candidates/models.py
@@ -6,11 +6,20 @@ from django.db import models
 # Create your models here.
 
 class District(models.Model):
-    zip_code = models.IntegerField();
-    congressional_district = models.IntegerField();
+    zip_code = models.IntegerField()
+    congressional_district = models.IntegerField()
 
     def __str__(self):
         return self.zip_code
 
-class CongressMan(model.Model):
-    
+class CongressMan(models.Model):
+    name = models.CharField(max_length=140)
+    stance = models.CharField(max_length=1)
+    party = models.CharField(max_length=15)
+    incumbent = models.CharField(max_length=3)
+    url = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=400)
+
+    def __str__(self):
+        return name
+
