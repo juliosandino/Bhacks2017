ba2cc83e97ea177e60551df6deff4fe775c0f922
Benson Gao <bnsn.gao@gmail.com>
2017-04-22 21:15:41 -0700

    added more views

--- src/candidates/views.py
+++ src/candidates/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def districts(request):
+	return render(request, 'districts.html', {})
+
+def about(request):
+	return render(request, 'about.html', {})
+
+def contact(request):
+	return render(request, 'contact.html', {})
